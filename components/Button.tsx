2ee1b551f170e01e7336d29163181f83fe1b3f00
marcoscorrales <marcosdecoa@gmail.com>
2024-02-21 12:31:32 +0100

    Navbar

---
+++ components/Button.tsx
@@ -0,0 +1,17 @@
+type ButtonProps = {
+    type: "button" | "submit";
+    title: string;
+    icon?: string;
+    variant: "btn_dark_green" | "btn_light_green" | "btn_dark_white";
+}
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
+        {icon && <img src={icon} alt={title} width={24} height={24} />}
+        <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  )
+}
+
+export default Button
